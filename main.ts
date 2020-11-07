da3acc400726873880d040cf6d49343fbe6750e3
Floran Hachez <floranhachez@rosa.be>
2020-11-07 20:31:25 +0100

    v0.1 of the iframe transformer

--- main.ts
+++ main.ts
@@ -1,6 +1,9 @@
-import { updateUrlIfYoutube } from 'iframe_converter';
-import { Plugin } from 'obsidian';
+
+import { App, Modal, Notice, Plugin } from 'obsidian';
+
+import { testSameOrigin, updateUrlIfYoutube } from 'iframe_converter';
 import { notionPathsToReferences } from 'regex';
+import { connect } from 'http2';
 
 function isUrl(text: string): boolean {
 	let urlRegex = new RegExp(
@@ -44,8 +47,13 @@ export default class FormatNotionPlugin extends Plugin {
 			: false;
 
 		if (selectedText && isUrl(selectedText)) {
+			console.log('same origin?', testSameOrigin(selectedText));
 			const url = updateUrlIfYoutube(selectedText)
-			editor.replaceSelection(`<iframe src="${url}" width="100%" height="500px">`);
+			const modal = new SampleModal(this.app, url, editor)
+			modal.open();
+			console.log(modal)
+		} else {
+			new Notice('Select an url to convert to an iframe.');
 		}
 	}
 
@@ -61,4 +69,124 @@ export default class FormatNotionPlugin extends Plugin {
 			editor.replaceSelection(updatedSelection);
 		}
 	}
+}
+
+class SampleModal extends Modal {
+	url: string
+	sucess: boolean
+	generatedIframe: string
+	editor: any
+
+	constructor(app: App, url: string, editor: any) {
+		super(app);
+		this.url = url;
+		this.editor = editor;
+	}
+
+	onOpen() {
+		let { contentEl } = this;
+
+		const title = contentEl.createEl('h2');
+		title.innerText = "This is how the iframe is going to look (your can choose the size)";
+
+		const iframe = createIframeEl(contentEl, this.url);
+
+		const widthCheckbox = createShouldUseDefaultWidthCheckbox(iframe);
+
+		const okButton = contentEl.createEl('button');
+		okButton.setText('OK');
+		okButton.className = 'mod-warning';
+		okButton.onclick = (e) => {
+			e.preventDefault();
+
+			const generatedIframe = iframe.outerHTML;
+			this.editor.replaceSelection(generatedIframe);
+			this.close();
+		}
+
+		const cancelButton = contentEl.createEl('button');
+		cancelButton.setText('Cancel');
+		cancelButton.onclick = (e) => {
+			e.preventDefault();
+			this.close();
+		}
+
+		const buttonContainer = contentEl.createEl('div');
+		buttonContainer.className = 'button__container';
+		buttonContainer.appendChild(okButton);
+		buttonContainer.appendChild(cancelButton);
+
+		const container = contentEl.createEl('div');
+		container.className = 'iframe__container';
+		container.appendChild(title);
+		container.appendChild(widthCheckbox);
+		container.appendChild(iframe);
+		container.appendChild(buttonContainer);
+
+		contentEl.appendChild(container);
+	}
+
+	onClose() {
+		let { contentEl } = this;
+		contentEl.empty();
+	}
+}
+
+function createIframeEl(contentEl: HTMLElement, url: string): HTMLIFrameElement {
+	const iframe = contentEl.createEl('iframe');
+	iframe.src = url;
+	iframe.className = 'resize-vertical'
+	iframe.style.display = 'none'
+
+	iframe.onload = (ev) => {
+		console.log('iframe is loaded!');
+		var html = null;
+		try {
+			// deal with older browsers
+			var doc = iframe.contentDocument || iframe.contentWindow.document;
+			html = doc.body.innerHTML;
+		} catch (err) {
+			// do nothing
+		}
+
+		if (html) {
+			console.log('OK');
+		} else {
+			console.log('bad')
+		}
+		iframe.style.display = 'block'
+	}
+	return iframe
+}
+
+
+function createShouldUseDefaultWidthCheckbox(iframe: HTMLElement): HTMLDivElement {
+	const name = "shouldUseDefaultWidth";
+	const checkboxContainer = iframe.createEl('div');
+	const checkbox = iframe.createEl('input');
+	checkbox.type = 'checkbox';
+	checkbox.name = name;
+	checkbox.checked = true;
+
+	const label = iframe.createEl('label');
+	label.setAttribute('for', name);
+	label.innerText = 'Do you want to use the note default width?'
+
+	checkbox.onclick = (e) => {
+		console.log('click!!!')
+
+		if (checkbox.checked) {
+			iframe.className = 'resize-vertical'
+			iframe.style.width = '100%'
+		} else {
+			iframe.className = 'resize-both'
+		}
+	}
+
+	checkboxContainer.appendChild(checkbox);
+	checkboxContainer.appendChild(label);
+
+	return checkboxContainer;
+
+
 }
